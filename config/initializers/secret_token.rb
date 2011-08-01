b548d9fe24d1a3e69e8173f5ecb60565213ed93e
Lonie Fink <loniefink@gmail.com>
2011-08-01 16:25:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestlist0201::Application.config.secret_token = 'b1065e531f78c473ce3e7c4cd71d96ead1fb5748dae80481edc140c4caedcbce107a7ca793083c14c4c211144028abaedd74e378eabcb6b7dc161f2bfe605c18'
